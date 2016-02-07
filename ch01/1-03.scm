5465281cecf859c272fa2ac9b4c0279208fbf645
Tom Wadeson <tom@tomwadeson.com>
2016-02-07 17:19:44 +0000

    Refactor to use local definitons

--- ch01/1-03.scm
+++ ch01/1-03.scm
@@ -1,7 +1,13 @@
 (define (sum-of-squares-of-two-largest x y z)
-  (cond ((and (<= x y) (<= x z)) (sum-of-squares y z))
-        ((and (<= y x) (<= y z)) (sum-of-squares x z))
-        (else (sum-of-squares x y))))
 
-(define (sum-of-squares x y)
-  (+ (* x x) (* y y)))
+  (define (minimum x y z)
+    (cond ((and (<= x y) (<= x z)) x)
+          ((and (<= y x) (<= y z)) y)
+          (else z)))
+
+  (define (sum-of-squares x y)
+    (+ (* x x) (* y y)))
+
+  (cond ((= x (minimum x y z)) (sum-of-squares y z))
+        ((= y (minimum x y z)) (sum-of-squares x z))
+        (else                  (sum-of-squares x y))))
